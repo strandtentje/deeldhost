60392d549819ad5ad50e3389f28a6f3e6300b36e
Rob Tierolff <teh@borreh.nl>
2015-11-10 20:05:22 +0100

    Tweaked message viewing and posting

--- message/queries/Get Message.auto.sql
+++ message/queries/Get Message.auto.sql
@@ -1,1 +1,12 @@
-SELECT * FROM Message JOIN Login ON Message.author = Login.id;
+SELECT 
+	Message.id,
+	text,
+	username,
+	start,
+	DATE_FORMAT(start, '%Y-%m-%d') `date`, 
+	DATE_FORMAT(start, '%H:%i:%s') `time`
+FROM 
+	Message 
+	JOIN Login ON Message.author = Login.id
+ORDER BY 
+	start DESC;
